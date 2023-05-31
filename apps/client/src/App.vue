b6b5c1051d3af398498c7f9266622c4434a45ef1
Guus Hamann <guus@sparkenergy.io>
2023-05-31 14:08:32 +0200

    make form

--- apps/client/src/App.vue
+++ apps/client/src/App.vue
@@ -1,34 +1,39 @@
 <template>
     <div class="container flex flex-col text-gray-500">
-        <div id="themepicker">
-            <input type="radio" id="yellow" name="colors" checked />
-            <label for="yellow" />
-            <input type="radio" id="orange" name="colors" />
-            <label for="orange" />
-            <input type="radio" id="pink" name="colors" />
-            <label for="pink" />
-            <input type="radio" id="red" name="colors" />
-            <label for="red" />
-            <input type="radio" id="purple" name="colors" />
-            <label for="purple" />
-            <input type="radio" id="blue" name="colors" />
-            <label for="blue" />
-            <input type="radio" id="green" name="colors" />
-            <label for="green" />
+        <div class="flex align-right"></div>
+        <div class="flex justify-center mb-4">
+            <div class="text-6xl title grow">Todos</div>
+            <ThemePicker class="text-6xl title"></ThemePicker>
         </div>
-        <div class="flex justify-center">
-            <div class="text-6xl title">Todos</div>
+        <div>
+            <div>
+                <input
+                    required
+                    id="todoInput"
+                    @keyup.enter="addTodo()"
+                    type="text"
+                    v-model="todo"
+                    placeholder="What do you want to do?"
+                    class="bg-white focus:outline-none focus:shadow-outline border py-2 px-4 block w-full leading-5 appearance-none placeholder-gray-500 text-gray-900 placeholder-opacity-25"
+                />
+                <div class="formelement mt-4 gap-4 flex">
+                    <button class="rounded-full px-4 tag">Private</button>
+                    <button class="rounded-full px-4 tag">Sport</button>
+                    <button class="rounded-full px-4 tag">Work</button>
+                    <button class="rounded-full px-4 tag">Grocery</button>
+                </div>
+
+                <div class="formelement justify-center mt-4">
+                    <textarea
+                        placeholder="Enter additional info here..."
+                        rows="5"
+                        cols="33"
+                        class="focus:outline-none focus:shadow-outline border py-2 px-4 block w-full leading-5 appearance-none text-gray-900 placeholder-opacity-25"
+                    ></textarea>
+                </div>
+            </div>
         </div>
 
-        <div class="flex justify-center mt-4">
-            <input
-                @keyup.enter="addTodo()"
-                type="text"
-                v-model="todo"
-                placeholder="What do you want to do?"
-                class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 py-2 px-4 block w-full leading-5 appearance-none placeholder-gray-500 text-gray-900 placeholder-opacity-25"
-            />
-        </div>
         <div class="tabs-container flex flex-col justify-center mt-4 text-xl">
             <div class="tabs gap-4 flex content-start">
                 <div
@@ -75,6 +80,7 @@
 import { ref } from 'vue'
 import { useTodos, useCreateTodo, useDeleteTodo } from './Queries'
 import TodoItem from './TodoItem.vue'
+import ThemePicker from './ThemePicker.vue'
 const createTodo = useCreateTodo()
 const deleteTodo = useDeleteTodo()
 
@@ -88,6 +94,12 @@ const addTodo = () => {
 </script>
 
 <style>
+#todoInput:required:invalid ~ .formelement {
+    display: none;
+}
+#todoInput:hover > .formelement {
+    display: flex;
+}
 .tabs-container .tabs .tab {
     &.active {
         @apply font-bold;
@@ -97,89 +109,5 @@ const addTodo = () => {
         @apply underline;
         cursor: pointer;
     }
-}
-/* .tabs-container .tab-content {
-    @apply hidden;
-    &.active {
-        @apply block;
-    }
-} */
-[type='radio'] {
-    display: none;
-}
-label {
-    display: inline-block;
-    width: 30px;
-    height: 30px;
-    margin: 10px;
-    border-radius: 50%;
-    box-shadow: 0 0 0 2px #f9f9f9, 0 0 0 3px #ddd;
-    cursor: pointer;
-}
-[type='radio']:checked + label {
-    box-shadow: 0 0 0 2px #f9f9f9, 0 0 0 3px var(--colorMain);
-}
-#yellow + label {
-    background: #ffd94e;
-}
-#orange + label {
-    background: #f2921b;
-}
-#pink + label {
-    background: #ffa1ad;
-}
-#red + label {
-    background: #a4303f;
-}
-#purple + label {
-    background: #8c6796;
-}
-#blue + label {
-    background: #4ebcff;
-}
-#green + label {
-    background: #afc97e;
-}
-body:has(#yellow:checked),
-#yellow:checked + label {
-    --colorMain: #ffd94e;
-    --colorLight: #ffe37e;
-    --colorAccent: #ffedae;
-}
-body:has(#orange:checked),
-#orange:checked + label {
-    --colorMain: #f2921b;
-    --colorLight: #f5af59;
-    --colorAccent: #f9cd97;
-}
-body:has(#pink:checked),
-#pink:checked + label {
-    --colorMain: #ffa1ad;
-    --colorLight: #ffc3ca;
-    --colorAccent: #ffdce1;
-}
-body:has(#red:checked),
-#red:checked + label {
-    --colorMain: #a4303f;
-    --colorLight: #c57b84;
-    --colorAccent: #d5a0a7;
-}
-body:has(#purple:checked),
-#purple:checked + label {
-    --colorMain: #8c6796;
-    --colorLight: #ab90b2;
-    --colorAccent: #d5c7d8;
-}
-body:has(#blue:checked),
-#blue:checked + label {
-    --colorMain: #4ebcff;
-    --colorLight: #93daff;
-    --colorAccent: #ceecff;
-}
-body:has(#green:checked),
-#green:checked + label {
-    --colorMain: #afc97e;
-    --colorLight: #c4d7a1;
-    --colorAccent: #d3e1b8;
 }
 </style>
---
+++ apps/client/src/ThemePicker.vue
@@ -0,0 +1,107 @@
+<template>
+    <div id="themepicker">
+        <input type="radio" id="yellow" name="colors" checked />
+        <label for="yellow" />
+        <input type="radio" id="orange" name="colors" />
+        <label for="orange" />
+        <input type="radio" id="pink" name="colors" />
+        <label for="pink" />
+        <input type="radio" id="red" name="colors" />
+        <label for="red" />
+        <input type="radio" id="purple" name="colors" />
+        <label for="purple" />
+        <input type="radio" id="blue" name="colors" />
+        <label for="blue" />
+        <input type="radio" id="green" name="colors" />
+        <label for="green" />
+    </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+[type='radio'] {
+    display: none;
+}
+label {
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    margin: 10px;
+    border-radius: 50%;
+    box-shadow: 0 0 0 2px #f9f9f9, 0 0 0 3px #ddd;
+    cursor: pointer;
+}
+[type='radio']:checked + label {
+    box-shadow: 0 0 0 2px #f9f9f9, 0 0 0 3px var(--colorMain);
+}
+#yellow + label {
+    background: #ffd94e;
+}
+#orange + label {
+    background: #f2921b;
+}
+#pink + label {
+    background: #ffa1ad;
+}
+#red + label {
+    background: #a4303f;
+}
+#purple + label {
+    background: #8c6796;
+}
+#blue + label {
+    background: #4ebcff;
+}
+#green + label {
+    background: #afc97e;
+}
+[type='radio']:not(:checked) + label {
+    display: none;
+}
+#themepicker:hover > label {
+    display: inline-block;
+}
+</style>
+<style>
+body:has(#yellow:checked),
+#yellow:checked + label {
+    --colorMain: #ffd94e;
+    --colorLight: #ffe37e;
+    --colorAccent: #ffedae;
+}
+body:has(#orange:checked),
+#orange:checked + label {
+    --colorMain: #f2921b;
+    --colorLight: #f5af59;
+    --colorAccent: #f9cd97;
+}
+body:has(#pink:checked),
+#pink:checked + label {
+    --colorMain: #ffa1ad;
+    --colorLight: #ffc3ca;
+    --colorAccent: #ffdce1;
+}
+body:has(#red:checked),
+#red:checked + label {
+    --colorMain: #a4303f;
+    --colorLight: #c57b84;
+    --colorAccent: #d5a0a7;
+}
+body:has(#purple:checked),
+#purple:checked + label {
+    --colorMain: #8c6796;
+    --colorLight: #ab90b2;
+    --colorAccent: #d5c7d8;
+}
+body:has(#blue:checked),
+#blue:checked + label {
+    --colorMain: #4ebcff;
+    --colorLight: #93daff;
+    --colorAccent: #ceecff;
+}
+body:has(#green:checked),
+#green:checked + label {
+    --colorMain: #afc97e;
+    --colorLight: #c4d7a1;
+    --colorAccent: #d3e1b8;
+}
+</style>
